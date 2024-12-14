92d0857a2bf042bb744794bc90b9bc23278d7662
Thomas Dinh (Dinh Nguyen Son Tung) <work.dnstung@gmail.com>
2024-12-14 11:35:47 +0700

    implement ant-design layout into project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [
+    CommonModule,
+    NzIconModule,
+    NzLayoutModule,
+    NzMenuModule,
+    MainLayoutComponent,
+  ],
+  template: ` <app-main-layout></app-main-layout> `,
+  // templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'recursive-nav-angular-practice';
+  isCollapsed = false;
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'layout',
+    component: MainLayoutComponent,
+    title: 'Main Layout',
+  },
+  // { path: '', redirectTo: '/welcome' },
+  // {
+  //   path: 'welcome',
+  //   loadChildren: () =>
+  //     import('./pages/welcome/welcome.routes').then((m) => m.WELCOME_ROUTES),
+  // },
+];
