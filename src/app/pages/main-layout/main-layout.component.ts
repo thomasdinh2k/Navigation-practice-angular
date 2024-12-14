072063e22898a1edb17029ff03187814f28a65cf
Thomas Dinh (Dinh Nguyen Son Tung) <work.dnstung@gmail.com>
2024-12-14 12:05:42 +0700

    implement ant menu for multi-level menu

--- src/app/pages/main-layout/main-layout.component.ts
+++ src/app/pages/main-layout/main-layout.component.ts
@@ -4,11 +4,18 @@ import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [NzBreadCrumbModule, NzIconModule, NzMenuModule, NzLayoutModule],
+  imports: [
+    NzBreadCrumbModule,
+    NzIconModule,
+    NzMenuModule,
+    NzLayoutModule,
+    MenuComponent,
+  ],
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.scss'],
 })
