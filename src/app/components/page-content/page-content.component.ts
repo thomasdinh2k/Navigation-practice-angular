e08f66440d35df3e06a629af8ccc2976ef94786a
Thomas Dinh (Dinh Nguyen Son Tung) <work.dnstung@gmail.com>
2024-12-24 14:09:22 +0700

    add style for todo list

--- src/app/components/page-content/page-content.component.ts
+++ src/app/components/page-content/page-content.component.ts
@@ -1,23 +1,29 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ServicePracticeComponent } from './service-practice/service-practice.component';
+import { TodoComponent } from './todo/todo.component';
 import { TreeViewComponent } from './tree-view/tree-view.component';
 
 @Component({
   selector: 'app-page-content',
-  imports: [TreeViewComponent, ServicePracticeComponent],
+  imports: [TreeViewComponent, ServicePracticeComponent, TodoComponent],
   template: `
-    <p>page-content works!</p>
+    <div class="wrapper">
+      <app-todo />
+      <h3>Exact path: {{ currentPath }}</h3>
 
-    <h3>Exact path: {{ currentPath }}</h3>
+      <h1>TreeView:</h1>
+      <app-tree-view />
 
-    <h1>TreeView:</h1>
-    <app-tree-view />
-
-    <h1>Service Practice</h1>
-    <app-service-practice />
+      <h1>Service Practice</h1>
+      <app-service-practice />
+    </div>
   `,
   styles: `
+    .wrapper {
+      overflow: auto;
+      height: 100%;
+    }
     h3 {
       color: red;
       font-size: 2rem;
