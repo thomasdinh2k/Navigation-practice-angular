6c159e13902d2ba12f7c67cb962cc0f41b510f68
Thomas Dinh (Dinh Nguyen Son Tung) <work.dnstung@gmail.com>
2024-12-24 13:50:04 +0700

    WIP observable practice

--- src/app/components/page-content/page-content.component.ts
+++ src/app/components/page-content/page-content.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ObservablePracticeComponent } from './observable-practice/observable-practice.component';
 import { ServicePracticeComponent } from './service-practice/service-practice.component';
 import { TreeViewComponent } from './tree-view/tree-view.component';
 
 @Component({
   selector: 'app-page-content',
-  imports: [TreeViewComponent, ServicePracticeComponent],
+  imports: [
+    TreeViewComponent,
+    ServicePracticeComponent,
+    ObservablePracticeComponent,
+  ],
   template: `
-    <p>page-content works!</p>
-
     <h3>Exact path: {{ currentPath }}</h3>
 
     <h1>TreeView:</h1>
@@ -16,6 +19,7 @@ import { TreeViewComponent } from './tree-view/tree-view.component';
 
     <h1>Service Practice</h1>
     <app-service-practice />
+    <app-observable-practice />
   `,
   styles: `
     h3 {
